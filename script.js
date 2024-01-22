99cc8eb143588f91f2bc736fa645e3269f6dd13d
dajjal <turanarican1985@gmail.com>
2024-01-22 22:39:51 +0300

    m

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*************************************/
-/* THE FUNCTIONS call() METHOD       */
+/* THE bind() METHOD                 */
 /*************************************/
 
 const lufthansa = {
@@ -19,30 +19,50 @@ const lufthansa = {
   },
 };
 
-lufthansa.book(945, 'Halide Tuğba');
-// Halide Tuğba booked a seat on LH945 from Lufthansa
-console.log(lufthansa.bookings);
-// [ { flight: 'LH945', passengerName: 'Halide Tuğba' } ]
-
 const eurowings = {
   airline: 'Eurowings',
   iataCode: 'EW',
   bookings: [],
 };
 
+// does not immediately call the bound function
+
+const bookEW = lufthansa.book.bind(eurowings);
+
+bookEW(546, 'Halide Tuğba');
+// Halide Tuğba booked a seat on EW546 from Eurowings
+
 const book = lufthansa.book;
 
-book.call(eurowings, 45, 'Halide Tuğba');
-// Halide Tuğba booked a seat on EW45 from Eurowings
-console.log(eurowings.bookings);
-// [ { flight: 'EW45', passengerName: 'Halide Tuğba' } ]
+// some of the arguments can be set in stone beforehand
+const bookEW657 = book.bind(eurowings, 657);
+bookEW657('Turan ARICAN');
+// Turan ARICAN booked a seat on EW657 from Eurowings
 
-lufthansa.book.call(eurowings, 26, 'Turan ARICAN');
-// Turan ARICAN booked a seat on EW26 from Eurowings
-console.log(eurowings.bookings);
+// bind() method with event listeners
+// normally, the callback function's "this" refer to the
+// HTML element calling the function upon event
+
+lufthansa.numOfPlanes = 300;
+lufthansa.buyNewPlane = function () {
+  console.log(`number of planes before buy: ${this.numOfPlanes}`);
+  this.numOfPlanes++;
+  console.log(`a new one bought and now it is ${this.numOfPlanes}`);
+};
+
+const newPlaneBtnLF = document.querySelector('.lf-buy');
+
+newPlaneBtnLF.addEventListener('click', lufthansa.buyNewPlane.bind(lufthansa));
+/* number of planes before buy: 300
+   a new one bought and now it is 301
+*/
+
+eurowings.numOfPlanes = 120;
+
+const newPlaneBtnEW = document.querySelector('.ew-buy');
+
+newPlaneBtnEW.addEventListener('click', lufthansa.buyNewPlane.bind(eurowings));
 /*
-[
-  { flight: 'EW45', passengerName: 'Halide Tuğba' },
-  { flight: 'EW26', passengerName: 'Turan ARICAN' }
-]
+number of planes before buy: 120
+a new one bought and now it is 121
 */
