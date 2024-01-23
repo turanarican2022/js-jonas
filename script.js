520728f772fe07dc3d5086f2422ffc4179b2005a
dajjal <turanarican1985@gmail.com>
2024-01-23 18:22:08 +0300

    m

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
 'use strict';
 
 /********************************************/
-/* IMMEDIATELY INVOKED FUNCTION EXPRESSIONS */
+/* CLOSURES                                 */
 /********************************************/
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, passengerName) {
-    console.log(
-      `${passengerName} booked a seat on ${this.iataCode}${flightNum} from ${this.airline}`
-    );
-    this.bookings.push({
-      flight: `${this.iataCode}${flightNum}`,
-      passengerName,
-    });
-  },
+// a function remembers the variable environment of the context it was born in
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
 };
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-
-// being called only once and then disappears
-// we don't need to save it somewhere
-
-(function () {
-  console.log(
-    'This function runs immediately and cannot be called elsewhere again'
-  );
-})(); // This function runs immediately and cannot be called elsewhere again
-
-// also works with arrow functions
-(() => console.log('This will neither again runs'))(); // This will neither again runs
-
-// parameters can be used
-(myName => console.log(`Hello ${myName} only once!`))('Halit'); // Hello Halit only once!
+const book = secureBooking();
+book(); // 1 passengers
+book(); // 2 passengers
+book(); // 3 passengers
