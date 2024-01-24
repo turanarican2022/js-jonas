ec74c3189d3d21068f4e58627c8eeeff3d2712b1
dajjal <turanarican1985@gmail.com>
2024-01-24 18:40:16 +0300

    m

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
 'use strict';
 
-/********************************************/
-/* forEach(()=>{}) with maps and sets       */
-/********************************************/
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
 
-// with maps
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
 
-const currenciesMap = new Map([
-  ['USD', 'US Dollar'],
-  ['GBP', 'British Pound'],
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
 ]);
 
-console.log(currenciesMap);
-/*
-Map(3) {
-  'USD' => 'US Dollar',
-  'GBP' => 'British Pound',
-  'EUR' => 'Euro'
-}
-*/
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-currenciesMap.forEach((val, key, mapItself) => {
-  console.log(`${key} is short for ${val}`);
-});
-/*
-USD is short for US Dollar
-GBP is short for British Pound
-EUR is short for Euro
-*/
-
-// with sets
-const carsSet = new Set(['Citroen', 'Alfa Romeo', 'Mazda', 'Fiat']);
-carsSet.forEach((val, key, setItself) => {
-  console.log(`Number ${key} car I bought was ${val}`);
-});
-/*
-Number Citroen car I bought was Citroen
-Number Alfa Romeo car I bought was Alfa Romeo
-Number Mazda car I bought was Mazda
-Number Fiat car I bought was Fiat
-*/
+/////////////////////////////////////////////////
